2fd530051b8516bfe45ddf1a00420708b59bb54a
Ifeoma <osegboifeoma3@gmail.com>
2026-05-16 20:13:03 +0200

    feat: update fetch URL for quote retrieval

--- Frontend/script.js
+++ Frontend/script.js
@@ -3,7 +3,9 @@ const quoteText = document.getElementById("quote");
 
 button.addEventListener("click", async () => {
   try {
-    const response = await fetch("http://localhost:3000/");
+    const response = await fetch(
+      "http://l1077u8ddys8rtiyxz2w0v83.178.105.39.91.sslip.io/",
+    );
     const data = await response.text();
     quoteText.textContent = data;
   } catch (error) {
